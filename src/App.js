13db0b10b1214ffec75a30ac4905ddb15f36a5fd
Blue Cho <blue@socialclub.co.kr>
2022-08-03 19:04:25 +0900

    Modify : 브라우저 라투어 위로 올림

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import CustomRoute from "./routes/CustomRoute";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <CustomRoute />;
+  return (
+    <BrowserRouter>
+      <CustomRoute />
+    </BrowserRouter>
+  );
 }
 
 export default App;
