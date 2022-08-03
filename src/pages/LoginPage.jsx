d042fefec1ad03c9987844f270a957cd27098b70
Blue Cho <blue@socialclub.co.kr>
2022-08-03 19:05:16 +0900

    Modify: Ref 빼고 훅폼으로 수정

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,41 +1,68 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import Input from "../components/common/Input";
 import Button from "../components/common/Button";
 import styled from "styled-components";
 import CenterLayout from "../components/common/CenterLayout";
 import SizedBox from "../components/common/SizedBox";
 import Utils from "../utils/index";
+import { useForm } from "react-hook-form";
+import { loginApi } from "../apis/userApi";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  const idRef = useRef("");
-  const pwRef = useRef("");
-  const [disabled, setDisabled] = useState("disabled");
+  const [disabled, setDisabled] = useState(false);
+  const navigate = useNavigate();
 
-  const onClickLogin = () => {
-    console.log("로그인클릭");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    loginApi(data);
+    navigate("/todos");
   };
 
   const onClickSignUp = () => {
     console.log("회원가입 클릭");
+    navigate("/auth", { replace: true });
   };
+
+  const isEmail = Utils.checkEmail(watch("email", false));
+  const isPassword = Utils.checkPassword(watch("password", false));
+
+  useEffect(() => {
+    if (isEmail && isPassword) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [isEmail, isPassword]);
 
   return (
     <CenterLayout>
-      <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <h1>로그인 페이지</h1>
-        <Input placeholder={"아이디"} ref={idRef} />
+        <Input placeholder={"아이디"} register={register} name={"email"} />
         <SizedBox height={16} />
-        <Input placeholder={"비밀번호"} ref={pwRef} />
+        <Input
+          type={"password"}
+          placeholder={"비밀번호"}
+          register={register}
+          name={"password"}
+        />
         <SizedBox height={16} />
-        <Button title={"로그인"} onClick={onClickLogin} disabled={disabled} />
+        <Button type={"submit"} title={"로그인"} disabled={disabled} />
         <SizedBox height={16} />
-        <Button title={"회원가입"} onClick={onClickSignUp} />
-      </Container>
+        <Button type={"button"} title={"회원가입"} onClick={onClickSignUp} />
+      </Form>
     </CenterLayout>
   );
 }
 
-const Container = styled.form`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
 `;
