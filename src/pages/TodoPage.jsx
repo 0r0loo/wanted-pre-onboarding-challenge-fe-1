27432f280d95b0c68b134a0cede86c475f796eab
Blue Cho <blue@socialclub.co.kr>
2022-08-03 19:08:07 +0900

    Add : todo, todos 페이지 추가

---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoPage() {
+  return (
+    <>
+      <h1>투두 상세 페이지</h1>
+    </>
+  );
+}
+
+export default TodoPage;
---
+++ src/pages/TodosPage.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { getTodosApi } from "../apis/todoApi";
+
+function TodosPage() {
+  const getTodos = async () => {
+    const result = await getTodosApi();
+    console.log("-> result", result);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      <h1>투두리스트페이지</h1>
+      <Outlet />
+    </>
+  );
+}
+
+export default TodosPage;
