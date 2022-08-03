12430a35d6ef7191bf00529e338521b96a072ea8
Blue Cho <blue@socialclub.co.kr>
2022-08-03 19:09:03 +0900

    Add : 토큰에따른 라우터분기

--- src/routes/CustomRoute.jsx
+++ src/routes/CustomRoute.jsx
@@ -1,16 +1,38 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import SignUpPage from "../pages/SignUpPage";
+import TodosPage from "../pages/TodosPage";
+import TodoPage from "../pages/TodoPage";
 
 function CustomRoute() {
+  const { pathname } = useLocation();
+  const navigation = useNavigate();
+
+  const navi = useCallback(
+    (token) => {
+      if (token && ["/", "/auth"].includes(pathname)) {
+        navigation("/todos");
+      } else {
+        navigation("/");
+      }
+    },
+    [navigation, pathname]
+  );
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    navi(token);
+  }, [navi]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<LoginPage />} />
-        <Route path={"/auth"} element={<SignUpPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<LoginPage />} />
+      <Route path={"/auth"} element={<SignUpPage />} />
+      <Route path={"/todos"} element={<TodosPage />}>
+        <Route path={":id"} element={<TodoPage />} />
+      </Route>
+    </Routes>
   );
 }
 
